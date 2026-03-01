fe3f2f1e2e67efd7f31eeec4b6a48416f8413d39
Cory Frenette <cory@frenette.dev>
2026-02-28 23:39:22 -0500

    add LspLogClear usercommand to clear LSP logs

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -26,5 +26,13 @@
       };
       bashls.enable = true;
     };
+
+    postConfig = ''
+        vim.api.nvim_create_user_command("LspLogClear", function()
+      	  local lsplogpath = vim.fn.stdpath("state") .. "/lsp.log"
+      	  print(lsplogpath)
+      	  if io.close(io.open(lsplogpath, "w+b")) == false then vim.notify("Clearing LSP Log failed", vim.log.levels.WARN) end
+        end, { nargs = 0 })
+    '';
   };
 }
